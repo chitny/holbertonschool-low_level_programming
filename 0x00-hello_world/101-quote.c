1306e3f350d5293099357f9459def461501977bd
Ignacio Chitnisky <2771@holbertonschool.com>
2021-02-03 15:26:21 +0000

    Adding comments to the file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+ *main - stderr out a phrase
+ *Return: 1 as we ask in the exercise
+ */
 int main(void)
 {
 fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
