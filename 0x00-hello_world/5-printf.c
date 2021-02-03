87573e8763e923271c56e560b60e25ce6b076074
Ignacio Chitnisky <2771@holbertonschool.com>
2021-02-03 15:16:21 +0000

    Adding comment with main and Return

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * main - function, prints a phrase
+ * Return: 0 if everything is correct
+ */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
