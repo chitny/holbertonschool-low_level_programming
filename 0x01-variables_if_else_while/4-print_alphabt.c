9c992b51768ae8cd4b153d994ea50c27696f4d08
root <chitny@github.com>
2021-02-04 19:10:35 +0000

    test

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - a function to display the alphabet
+ *Return: 0 if everything is ok
+ */
+
+int main(void)
+{
+	char ch='a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar (ch);
+		}
+		ch++;
+	}
+	putchar ('\n');
+	return (0);
+}
