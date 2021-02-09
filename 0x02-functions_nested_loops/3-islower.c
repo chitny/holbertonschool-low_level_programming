90e8a6504e21f0a88e3c982967f2407e5e330aa1
Ignacio Chitnisky <2771@holbertonschool.com>
2021-02-09 12:59:40 +0000

    adding from 0 to 5 exercises

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for lowercase character
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c <= 122 && c >= 97)
+		return (1);
+	else
+		return (0);
+}
