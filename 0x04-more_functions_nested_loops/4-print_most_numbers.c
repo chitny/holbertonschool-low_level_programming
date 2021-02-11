3fe0d22cf1f5e389c3239bccbcbf54fc8576239c
Ignacio Chitnisky <2771@holbertonschool.com>
2021-02-11 17:55:42 +0000

    adding 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 but not 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i + '0');
+		}
+		else
+		{
+		i++
+		}
+	}
+
+	_putchar('\n');
+}
