1f23ffbe295fa585fb49b76ac40b43b38bde291f
Ignacio Chitnisky <2771@holbertonschool.com>
2021-02-11 17:51:44 +0000

    rewrite everything

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 
 /**
- * print_numbers - function that prints the numbers, from 0 to 9.
+ * print_numbers - prints the numbers, from 0 to 9
  */
-
 void print_numbers(void)
 {
-	int = i;
+	int i;
 
 	for (i = 0; i < 10; i++)
-	_putchar(i + '0');
+	{
+		_putchar(i + '0');
+	}
 
 	_putchar('\n');
 }
