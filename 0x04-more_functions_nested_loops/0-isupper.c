51c1df0712241d9ca42dc931d7c5e30c37fbdaa5
Ignacio Chitnisky <2771@holbertonschool.com>
2021-02-11 17:00:54 +0000

    first exercise

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,13 @@
 
 /**
  *int_isupper - function to checks for uppercase
- *c - variable to check
+ *@c: variable to check
+ *Return: 1 or 0
  */
 
 int _isupper(int c);
 {
-	if (c => 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
