bd9fc97186d5662e79e465d037ae4e1955144866
Ignacio Chitnisky <2771@holbertonschool.com>
2021-02-11 18:54:02 +0000

    add 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *print_line - function that draws a straight line
+ *@n: number of _
+ */
+
+void print_line(int n)
+{
+	int j;
+
+	if (n > 0)
+	{
+		for (j = 0; j <= n ; j++)
+		{
+			_putchar('_');
+		}
+	}
+
+		_putchar('\n');
+}
