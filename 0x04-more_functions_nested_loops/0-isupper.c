e7f961746ad95814cab5fbea3f1142506e2583f9
Ignacio Chitnisky <2771@holbertonschool.com>
2021-02-11 17:13:51 +0000

    changing from numbers to letters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  *int_isupper - function to checks for uppercase
@@ -9,7 +8,7 @@
 
 int _isupper(int c);
 {
-	if (c >= "65" && c <= "90")
+	if (c >= "A" && c <= "Z")
 		return (1);
 	else
 		return (0);
