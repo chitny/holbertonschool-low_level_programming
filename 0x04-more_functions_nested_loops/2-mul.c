9693a0342bb1defd040fbcbc0e2222a076d305f9
Ignacio Chitnisky <2771@holbertonschool.com>
2021-02-11 17:34:05 +0000

    adding 2nd exercise

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code for Holberton School students.
+ *@a: first multiple
+ *@b: second multiple
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
