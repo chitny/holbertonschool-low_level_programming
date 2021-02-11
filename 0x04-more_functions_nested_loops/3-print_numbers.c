0cf86be31d2381ba4db2f6ef5d8bf5c63a7217ce
Ignacio Chitnisky <2771@holbertonschool.com>
2021-02-11 17:50:27 +0000

    removing return comment

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,13 @@
 
 /**
  * print_numbers - function that prints the numbers, from 0 to 9.
- * Return: Always 0.
  */
 
 void print_numbers(void)
 {
 	int = i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	_putchar(i + '0');
 
 	_putchar('\n');
