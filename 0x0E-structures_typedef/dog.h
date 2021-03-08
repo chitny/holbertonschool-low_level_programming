101e0c0ce938a274e93dfcbd33a73f5c74eb590e
chitny <chitny@github.com>
2021-03-08 19:20:46 +0000

    fixing doggy struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,20 @@
-#ifndef dog
-#define dog
+#ifndef DOGGY
+#define DOGGY
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
-char *name
-float age
-char *owner
-}
+char *name;
+float age;
+char *owner;
+};
+
 #endif
