6f11d8f5475134cce873a30b98555aba820344a6
chitny <chitny@github.com>
2021-03-11 16:39:56 +0000

    fixing 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - Write a function that prints numbers, followed by a new line.
+ * print_numbers - a function that prints numbers, followed by a new line.
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  */
