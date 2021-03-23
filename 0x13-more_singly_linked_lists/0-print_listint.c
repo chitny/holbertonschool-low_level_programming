8088e674c9a0237cdce1cb4d915aa84f692885cb
Ignacio Chitnisky <2771@holbertonschool.com>
2021-03-23 20:29:29 +0000

    second try

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,14 +7,20 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	int i;
+	listint_t *search = (listint_t *)h;
 
-	while (h)
+	if (!h)
+		return (0);
+
+	for (i = 0; search; i++)
 	{
-		printf("%d\n", h->n);
-		num++;
-		h = h->next;
+		if (!search->n)
+			printf("0\n");
+		else
+			printf("%i\n", search->n);
+		search = search->next;
 	}
 
-	return (num);
+	return (i);
 }
