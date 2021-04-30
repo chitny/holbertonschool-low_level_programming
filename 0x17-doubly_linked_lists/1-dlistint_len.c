cd77c6a9dd148c0a9a5651aee8bad13387e8a9a2
Ignacio Chitnisky <elinmortal@gmail.com>
2021-04-30 04:19:12 +0000

    first two

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - a function that prints the number of the elements.
+ * @h: pointer to the dlistint_t
+ *
+ * Return: the number of nodes.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t size = 0;
+
+	if (!h)
+        return(0);
+    
+    while (h)
+	{
+	    h = h->next;
+        size++;
+    }
+
+	return (size);
+
+}
