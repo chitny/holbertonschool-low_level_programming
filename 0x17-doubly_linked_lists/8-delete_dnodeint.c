19d6c4cb709a04c93b5cb02f3934f05826aa7a3a
Ignacio Chitnisky <elinmortal@gmail.com>
2021-04-30 14:42:24 +0000

    changing 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,25 +1,27 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of nodes in a doubly linked list
- * @h: pointer to the linkde list
+ * dlistint_len - a function that prints the number of the elements.
+ * @h: pointer to the dlistint_t
  *
- * Return: number of nodes
+ * Return: the number of nodes.
  */
+
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	size_t size = 0;
 
 	if (!h)
 		return (0);
 
 	while (h)
 	{
-		nodes++;
 		h = h->next;
+		size++;
 	}
 
-	return (nodes);
+	return (size);
+
 }
 
 /**
