9b7c8b3d7bb96cdfe4cdb2f9d0c274c6f3b97382
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-03 17:44:54 +0000

    modify of .sh file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/chitny/holbertonschool-low_level_programming/tree/main/0x18-dynamic_libraries/hack.so
+wget -o /tmp/hack.so https://github.com/chitny/holbertonschool-low_level_programming/tree/main/0x18-dynamic_libraries/hack.so
 export LD_PRELOAD=/tmp/hack.so
