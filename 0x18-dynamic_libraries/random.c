c52e18b235d63980e852a2c0236a1b2e68aef0d4
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-03 17:33:18 +0000

    tired of this

--- 0x18-dynamic_libraries/random.c
+++ 0x18-dynamic_libraries/random.c
@@ -1,8 +1,14 @@
+/**
+ * rand - check the code for Holberton School students.
+ * Return: Always number of Lottery.
+ */
+
 int rand(void)
+
 {
 	static int i = -1;
 	int randomn[6] = {8, 8, 7, 9, 23, 74};
 
 	i++;
-	return(randomn[i]);
+	return (randomn[i]);
 }
