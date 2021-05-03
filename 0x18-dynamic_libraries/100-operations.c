d0566aa3b033c9d6b072e34f247e937413e15d21
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-03 16:21:18 +0000

    first advanced

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - check the code for Holberton School students.
+ *@a: first multiple
+ *@b: second multiple
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - check the code for Holberton School students.
+ *@a: first multiple
+ *@b: second multiple
+ * Return: Always 0.
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - check the code for Holberton School students.
+ *@a: first multiple
+ *@b: second multiple
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - check the code for Holberton School students.
+ *@a: first multiple
+ *@b: second multiple
+ * Return: Always 0.
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - check the code for Holberton School students.
+ *@a: first multiple
+ *@b: second multiple
+ * Return: Always 0.
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
