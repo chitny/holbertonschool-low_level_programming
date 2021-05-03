1be7204f704c83db1f878a0f05cd3f32d65d55f4
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-03 17:18:28 +0000

    trying again

--- 0x18-dynamic_libraries/script.sh
+++ 0x18-dynamic_libraries/script.sh
@@ -1,6 +1,6 @@
 alias md5sum='printf "$(md5sum gm)\n"'
 echo -n 'main(){write(1,"9 8 10 24 75 - 9\nCongratulations, you win' > gm.c
-echo 'the Jackpot!\n",55);return(0);}' > gm.c
+echo -n 'the Jackpot!\n",55);return(0);}' > gm.c
 gcc gm.c -o gm
 rm gm.c
 rm script.sh
