468463fc341c097e76d888d8e54256cbb25215df
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-28 23:36:49 +0000

    fixing 0 and 3

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,26 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array of the hash table
- *
- * Return: Pointer to the newly created hash table
+ * hash_table_create - Creates a hash table
+ * @size: Size of the hash table
+ * -------------------------
+ * Return: The direction to a new table of size SIZE
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
-	if (size == 0)
+	if (!new_table)
 		return (NULL);
-
-	ht = malloc(sizeof(hash_table_t));
-	if (!ht)
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t **) * size);
+	if (!new_table->array)
+	{
+		free(new_table);
 		return (NULL);
+	}
 
-	ht->size = size;
-	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
-	if (ht->array == NULL)
-		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		new_table->array[i] = NULL;
+	}
 
-	return (ht);
+	return (new_table);
 }
